8cceec8726306db8f4c29d1a937d3473c9f15f4e
Eduardo Passarelli <epassarelli@gmail.com>
2024-05-28 20:30:15 -0300

    Se agrega mitrait que venia de Livewire

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,15 @@ use App\Http\Controllers\ShowsController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ImagenController;
 
-use App\Http\Controllers\Dashboard\InterpretesCRUD;
+// use App\Http\Controllers\Dashboard\InterpretesCRUD;
 
 // use App\Http\Controllers\VideosController;
-// use App\Http\Livewire\Backend\Dashboard;
-// use App\Http\Livewire\Backend\Noticias;
-// use App\Http\Livewire\Backend\Interpretes;
-// use App\Http\Livewire\Backend\Users;
+use App\Http\Livewire\Backend\Dashboard;
+use App\Http\Livewire\Backend\Noticias;
+use App\Http\Livewire\Backend\Interpretes;
+use App\Http\Livewire\Backend\Users;
 
-Route::resource('dashboard/interpretes', InterpretesCRUD::class);
+// Route::resource('dashboard/interpretes', InterpretesCRUD::class);
 
 
 // Rutas para el controlador de biografia
@@ -99,31 +99,31 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 // });
 
 
-// Route::middleware([
-//     'auth:sanctum',
-//     config('jetstream.auth_session'),
-//     'verified'
-// ])->group(function () {
+Route::middleware([
+  'auth:sanctum',
+  config('jetstream.auth_session'),
+  'verified'
+])->group(function () {
 
-//     Route::prefix('admin')->group(function () {
-//         Route::get('/dashboard', Dashboard::class)->name('admin.dashboard');
-//         Route::get('/usuarios', Users::class)->name('admin.usuarios');
-//         Route::get('/interpretes', Interpretes::class)->name('admin.interpretes');
-//         Route::get('/noticias', Noticias::class)->name('admin.noticias');
-// Route::get('/noticias', Noticias::class)->name('admin.shows');
-// Route::get('/noticias', Noticias::class)->name('admin.discos');
-// Route::get('/noticias', Noticias::class)->name('admin.canciones');
-// Route::get('/noticias', Noticias::class)->name('admin.entrevistas');
-// Route::get('/noticias', Noticias::class)->name('admin.videos');
+  Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', Dashboard::class)->name('admin.dashboard');
+    Route::get('/usuarios', Users::class)->name('admin.usuarios');
+    Route::get('/interpretes', Interpretes::class)->name('admin.interpretes');
+    //         Route::get('/noticias', Noticias::class)->name('admin.noticias');
+    // Route::get('/noticias', Noticias::class)->name('admin.shows');
+    // Route::get('/noticias', Noticias::class)->name('admin.discos');
+    // Route::get('/noticias', Noticias::class)->name('admin.canciones');
+    // Route::get('/noticias', Noticias::class)->name('admin.entrevistas');
+    // Route::get('/noticias', Noticias::class)->name('admin.videos');
 
-// Route::get('/noticias', Noticias::class)->name('admin.festivales');
-// Route::get('/noticias', Noticias::class)->name('admin.radios');
-// Route::get('/noticias', Noticias::class)->name('admin.penias');
-// Route::get('/noticias', Noticias::class)->name('admin.comidas');
-// Route::get('/noticias', Noticias::class)->name('admin.mitos');
-// Route::get('/articulos', Noticias::class)->name('admin.articulos');
-//     });
-// });
+    // Route::get('/noticias', Noticias::class)->name('admin.festivales');
+    // Route::get('/noticias', Noticias::class)->name('admin.radios');
+    // Route::get('/noticias', Noticias::class)->name('admin.penias');
+    // Route::get('/noticias', Noticias::class)->name('admin.comidas');
+    // Route::get('/noticias', Noticias::class)->name('admin.mitos');
+    // Route::get('/articulos', Noticias::class)->name('admin.articulos');
+  });
+});
 
 
 
