f6a1528eb1fc36098adb0d1c90a2861b3666f2d7
Eduardo Passarelli <epassarelli@gmail.com>
2024-06-01 11:12:28 -0300

    Correciones en el commit y merge

--- routes/admin.php
+++ routes/admin.php
@@ -1,41 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-
-use App\Http\Livewire\Backend\Noticias;
-use App\Http\Livewire\Backend\Interpretes;
-use App\Http\Livewire\Backend\Users;
-
-Route::middleware([
-  'auth:sanctum',
-  config('jetstream.auth_session'),
-  'verified'
-])->group(function () {
-
-  Route::prefix('admin')->group(function () {
-    Route::get('/usuarios', Users::class)->name('admin.usuarios');
-    Route::get('/interpretes', Interpretes::class)->name('admin.interpretes');
-    Route::get('/noticias', Noticias::class)->name('admin.noticias');
-    // Route::get('/noticias', Noticias::class)->name('admin.shows');
-    // Route::get('/noticias', Noticias::class)->name('admin.discos');
-    // Route::get('/noticias', Noticias::class)->name('admin.canciones');
-    // Route::get('/noticias', Noticias::class)->name('admin.entrevistas');
-    // Route::get('/noticias', Noticias::class)->name('admin.videos');
-
-    // Route::get('/noticias', Noticias::class)->name('admin.festivales');
-    // Route::get('/noticias', Noticias::class)->name('admin.radios');
-    // Route::get('/noticias', Noticias::class)->name('admin.penias');
-    // Route::get('/noticias', Noticias::class)->name('admin.comidas');
-    // Route::get('/noticias', Noticias::class)->name('admin.mitos');
-    // Route::get('/articulos', Noticias::class)->name('admin.articulos');
-  });
-
-  // Route::get('dashboard', function () {
-  //   return view('dashboard');
-  // })->name('dashboard');
-});
-=======
 use App\Http\Controllers\Backend\NoticiaController;
 
 
@@ -59,4 +24,3 @@ Route::resource('admin/noticias', NoticiaController::class)->names([
   'update' => 'crud.noticias.update',
   'destroy' => 'crud.noticias.destroy',
 ]);
->>>>>>> dev
