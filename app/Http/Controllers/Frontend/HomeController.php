10b75a45e8f7b38004a2703f2958940a8ed93c2e
Eduardo Passarelli <epassarelli@gmail.com>
2024-07-04 09:56:09 -0300

    Nueva portada con contenidos dinamicos. Noticias, Shows, Discos, Biografias y Letras. Aun restan detalles esteticos v0.2

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -9,23 +9,36 @@ use Illuminate\Http\Request;
 use App\Models\Interprete;
 use App\Models\Noticia;
 use App\Models\Show;
-use App\Models\Disco;
+use App\Models\Album;
 use App\Models\Cancion;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        // Obtener las últimas 4 noticias
         $noticia = new Noticia();
-        // Obtener los últimos 5 intérpretes
-        $noticiasDestacadas = $noticia->getNLast(Noticia::class, 4);
+        $noticias = $noticia->getNLast(Noticia::class, 4);
 
+        // Obtener las últimas 4 shows
+        $show = new Show();
+        $shows = $show->getNLast(Show::class, 4);
+
+        // Obtener los últimos 3 intérpretes
         $interprete = new Interprete();
-        // Obtener los últimos 5 intérpretes
-        $artistas = $interprete->getNLast(Interprete::class, 6);
+        $interpretes = $interprete->getNLast(Interprete::class, 4);
+
+        // Obtener los últimos 3 intérpretes
+        $disco = new Album();
+        $discos = $disco->getNLast(Album::class, 4);
+
+        // Obtener los últimos 3 intérpretes
+        $cancion = new Cancion();
+        $canciones = $cancion->getNLast(Cancion::class, 6);
 
         $metaTitle = "Mi Folklore Argentino | Todo sobre Nuestras Tradiciones y Costumbres";
         $metaDescription = "Bienvenido a Mi Folklore Argentino, tu portal sobre la cultura y tradiciones de Argentina. Descubre música, danzas y más. ¡Visítanos hoy!";
-        return view('frontend.home', compact('metaTitle', 'metaDescription', 'noticiasDestacadas', 'artistas'));
+
+        return view('frontend.home', compact('metaTitle', 'metaDescription', 'noticias', 'interpretes', 'shows', 'discos', 'canciones'));
     }
 }
--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -155,20 +155,180 @@
 @endsection --}}
 
 @section('content')
-  <div class="container">
-    <!-- Introducción -->
-    {{-- <section class="intro mb-5">
-      <h1 class="mb-4">Bienvenidos a Nuestro Portal de Folklore Argentino</h1>
-      <p>Descubre las últimas noticias, eventos, letras de canciones, biografías de artistas y mucho más sobre el folklore
-        argentino. Nuestro objetivo es preservar y promover la rica cultura del folklore en Argentina.</p>
-    </section> --}}
 
+  {{-- <div class="bg-slate-400" style="background-color: gray">
+    <div class="container mt-4">
+      <!-- Primer Carousel -->
+      <h2>Relacionado con tus visitas en Juegos y Juguetes</h2>
+      <div id="carouselExampleIndicators1" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators1" data-bs-slide-to="0" class="active"
+            aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators1" data-bs-slide-to="1"
+            aria-label="Slide 2"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="row">
+              <!-- Aquí van las cards del primer slide -->
+              <div class="col-md-3 m-2">
+                <div class="card h-100">
+                  <img src="{{ asset('img/biografias-folkloricas.jpg') }}" alt="Biografias de folklore"
+                    class="card-img-top">
+                  <div class="card-body">
+                    <h5 class="card-title" style="font-size: 14px;">Bloques Para Armar Lego 42132 Technic Moto</h5>
+                    <p class="card-text" style="font-size: 20px; font-weight: bold;">$45.500</p>
+                    <p class="card-text" style="font-size: 14px;">en 6 cuotas de $10.033</p>
+                    <p class="card-text" style="font-size: 14px; color: green;">Llega gratis mañana</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-3">
+                <div class="card h-100">
+                  <img src="{{ asset('img/biografias-folkloricas.jpg') }}" alt="Biografias de folklore"
+                    class="card-img-top">
+                  <div class="card-body">
+                    <h5 class="card-title" style="font-size: 14px;">Lego Technic - Jeep Wrangler</h5>
+                    <p class="card-text" style="font-size: 20px; font-weight: bold;">$199.800</p>
+                    <p class="card-text" style="font-size: 14px;">10% OFF</p>
+                    <p class="card-text" style="font-size: 14px; color: green;">Llega gratis mañana</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-3">
+                <div class="card h-100">
+                  <img src="{{ asset('img/biografias-folkloricas.jpg') }}" alt="Biografias de folklore"
+                    class="card-img-top">
+                  <div class="card-body">
+                    <h5 class="card-title" style="font-size: 14px;">Lego Technic - Jeep Wrangler</h5>
+                    <p class="card-text" style="font-size: 20px; font-weight: bold;">$199.800</p>
+                    <p class="card-text" style="font-size: 14px;">10% OFF</p>
+                    <p class="card-text" style="font-size: 14px; color: green;">Llega gratis mañana</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-3">
+                <div class="card h-100">
+                  <img src="{{ asset('img/biografias-folkloricas.jpg') }}" alt="Biografias de folklore"
+                    class="card-img-top">
+                  <div class="card-body">
+                    <h5 class="card-title" style="font-size: 14px;">Lego Technic - Jeep Wrangler</h5>
+                    <p class="card-text" style="font-size: 20px; font-weight: bold;">$199.800</p>
+                    <p class="card-text" style="font-size: 14px;">10% OFF</p>
+                    <p class="card-text" style="font-size: 14px; color: green;">Llega gratis mañana</p>
+                  </div>
+                </div>
+              </div>
+
+
+              <!-- Añade más col-md-2 con cards según sea necesario -->
+            </div>
+          </div>
+
+
+          <div class="carousel-item">
+            <div class="row">
+              <!-- Aquí van las cards del segundo slide -->
+              <div class="col-md-2">
+                <div class="card h-100">
+                  <img src="{{ asset('img/biografias-folkloricas.jpg') }}" alt="Biografias de folklore"
+                    class="card-img-top">
+                  <div class="card-body">
+                    <h5 class="card-title" style="font-size: 14px;">Lego Technic - Jeep Wrangler</h5>
+                    <p class="card-text" style="font-size: 20px; font-weight: bold;">$199.800</p>
+                    <p class="card-text" style="font-size: 14px;">10% OFF</p>
+                    <p class="card-text" style="font-size: 14px; color: green;">Llega gratis mañana</p>
+                  </div>
+                </div>
+              </div>
+              <!-- Añade más col-md-2 con cards según sea necesario -->
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators1"
+          data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators1"
+          data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+
+
+
+      <!-- Segundo Carousel -->
+      <div class="card my-4 p-4">
+        <h2>Visto recientemente</h2>
+        <div id="carouselExampleIndicators2" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="0" class="active"
+              aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="1"
+              aria-label="Slide 2"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="row">
+                <!-- Aquí van las cards del primer slide -->
+                <div class="col-md-2">
+                  <div class="card h-100">
+                    <img src="path_to_image3.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title" style="font-size: 14px;">Cpu</h5>
+                      <p class="card-text" style="font-size: 20px; font-weight: bold;">$250.000</p>
+                    </div>
+                  </div>
+                </div>
+                <!-- Añade más col-md-2 con cards según sea necesario -->
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="row">
+                <!-- Aquí van las cards del segundo slide -->
+                <div class="col-md-2">
+                  <div class="card h-100">
+                    <img src="path_to_image4.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title" style="font-size: 14px;">Escritorio Plegable Pared Rebatible</h5>
+                      <p class="card-text" style="font-size: 20px; font-weight: bold;">$93.738</p>
+                      <p class="card-text" style="font-size: 14px;">5% OFF</p>
+                      <p class="card-text" style="font-size: 14px; color: green;">Envío gratis</p>
+                    </div>
+                  </div>
+                </div>
+                <!-- Añade más col-md-2 con cards según sea necesario -->
+              </div>
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators2"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators2"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div> --}}
+
+  <div class="container">
     <!-- Noticias Destacadas -->
     <section class="noticias-destacadas mb-5">
-      <h1 class="mb-4">Bienvenidos al Portal del Folklore Argentino</h1>
+      {{-- <h1 class="mb-4">Bienvenidos al Portal del Folklore Argentino</h1> --}}
       <div id="noticiasCarousel" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
-          @foreach ($noticiasDestacadas as $index => $noticia)
+          @foreach ($noticias as $index => $noticia)
             <div class="carousel-item @if ($index == 0) active @endif">
               <img src="{{ asset('storage/noticias/' . $noticia->foto) }}" class="d-block w-100"
                 alt="{{ $noticia->titulo }}">
@@ -192,57 +352,121 @@
     </section>
 
     <!-- Próximos Shows y Festivales -->
-    {{-- <section class="shows-festivales mb-5">
+    <section class="shows-festivales mb-5">
       <h2 class="mb-4">Próximos Shows y Festivales de Folklore Argentino</h2>
       <div class="row">
-        @foreach ($eventos as $evento)
-          <div class="col-md-4 mb-4">
-            <div class="card h-100">
-              <img src="{{ asset('storage/' . $evento->imagen) }}" class="card-img-top" alt="{{ $evento->nombre }}">
-              <div class="card-body">
-                <h4 class="card-title">{{ $evento->nombre }}</h4>
-                <p class="card-text">{{ $evento->fecha }} - {{ $evento->lugar }}</p>
+        @foreach ($shows as $evento)
+          <div class="col-md-6">
+            <div class="card mb-3 bg-slate-200">
+              <div class="row g-0">
+                <div class="col-md-4 col-lg-2 d-flex align-items-center justify-content-center bg-light">
+                  <div class="text-center">
+                    <h5 class="card-title mb-1">{{ date('d', strtotime($evento->fecha)) }}</h5>
+                    <p class="card-text mb-1">{{ date('M', strtotime($evento->fecha)) }}</p>
+                    <p class="card-text">{{ date('Y', strtotime($evento->fecha)) }}</p>
+                  </div>
+                </div>
+                <div class="col-md-8 col-lg-10">
+                  <div class="card-body">
+                    <div class="d-flex align-items-center mb-2">
+                      <img src="{{ asset('storage/interpretes/' . $evento->interprete->foto) }}"
+                        alt="{{ $evento->interprete->interprete }}" class="rounded-circle me-3"
+                        style="width: 50px; height: 50px;">
+                      <h5 class="card-title m-0">{{ $evento->interprete->interprete }}</h5>
+                    </div>
+                    <h6 class="card-subtitle mb-2 text-muted"><strong>Show:</strong> {{ $evento->show }}</h6>
+                    {{-- <p class="card-text"><strong>Show:</strong> {{ $evento->show }}</p> --}}
+                    <p class="card-text"><strong>Detalles:</strong> {!! $evento->detalle !!}</p>
+                    <p class="card-text"><strong>Lugar:</strong> {{ $evento->lugar }}</p>
+                    <p class="card-text">{{ $evento->descripcion }}</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         @endforeach
       </div>
-    </section> --}}
+    </section>
 
-    <!-- Letras de Canciones Populares -->
-    {{-- <section class="letras-canciones mb-5">
-      <h2 class="mb-4">Letras de Canciones Populares del Folklore Argentino</h2>
-      <div class="row">
-        @foreach ($canciones as $cancion)
-          <div class="col-md-4 mb-4">
-            <div class="card h-100">
-              <img src="{{ asset('storage/' . $cancion->imagen) }}" class="card-img-top" alt="{{ $cancion->titulo }}">
-              <div class="card-body">
-                <h4 class="card-title">{{ $cancion->titulo }}</h4>
-                <p class="card-text">{{ Str::limit($cancion->letra, 100) }}</p>
-                <a href="{{ route('canciones.show', $cancion->id) }}" class="btn btn-primary">Leer más</a>
-              </div>
-            </div>
-          </div>
-        @endforeach
-      </div>
-    </section> --}}
+
 
     <!-- Biografías de Artistas -->
     <section class="biografias-artistas mb-5">
       <h2 class="mb-4">Biografías de Artistas del Folklore Argentino</h2>
       <div class="row">
-        @foreach ($artistas as $artista)
-          <div class="col-md-4 mb-4">
-            <div class="card h-100">
-              <img src="{{ asset('storage/interpretes/' . $artista->imagen) }}" class="card-img-top"
-                alt="{{ $artista->nombre }}">
+        @foreach ($interpretes as $artista)
+          <div class="col-md-3 mb-4">
+            <div class="card h-150">
+              <img src="{{ asset('storage/interpretes/' . $artista->foto) }}" class="card-img-top"
+                alt="{{ $artista->interprete }}">
               <div class="card-body">
                 <h4 class="card-title">{{ $artista->interprete }}</h4>
-                <p class="card-text">{{ Str::limit($artista->biografia, 100) }}</p>
+                {{-- <p class="card-text">{{ Str::limit($artista->biografia, 100) }}</p> --}}
                 <a href="{{ route('interpretes.index', $artista->id) }}" class="btn btn-primary">Leer más</a>
               </div>
             </div>
+          </div>
+        @endforeach
+      </div>
+    </section>
+
+
+    <!-- Letras de Canciones Populares -->
+    <section class="letras-canciones mb-5">
+      <h2 class="mb-4">Letras de Canciones Populares del Folklore Argentino</h2>
+      <div class="row">
+        @foreach ($canciones as $cancion)
+          <div class="col-md-4 mb-4">
+            <a href="{{ route('canciones.show', [$cancion->interprete->slug, $cancion->slug]) }}"
+              class="card h-100 shadow-sm text-decoration-none">
+              <div class="row g-0">
+                <div class="col-auto">
+                  @if (file_exists(public_path('storage/interpretes/' . $cancion->interprete->foto)) && $cancion->interprete->foto !== '')
+                    <img class="img-fluid rounded-start"
+                      src="{{ asset('storage/interpretes/' . $cancion->interprete->foto) }}"
+                      alt="{{ $cancion->interprete }}" style="width: 6rem; height: auto; object-fit: cover;">
+                  @else
+                    <img src="{{ asset('storage/img/imagennodisponible600x400.jpg') }}" alt="Imagen no disponible"
+                      class="card-img-top" style="width: 6rem; height: auto; object-fit: cover;">
+                  @endif
+                </div>
+                <div class="col">
+                  <div class="card-body">
+                    <h2 class="card-title h6 text-dark mb-2 hover:text-primary">
+                      {{ $cancion->cancion }}
+                    </h2>
+                    <p class="card-text text-muted mb-2">
+                      {{ $cancion->interprete->interprete }}
+                    </p>
+                    <p class="card-text">{{ number_format($cancion->visitas, 0, '', ',') }} visitas</p>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        @endforeach
+      </div>
+    </section>
+
+
+    <!-- Letras de Canciones Populares -->
+    <section class="letras-canciones mb-5">
+      <h2 class="mb-4">Discos del Folklore Argentino agregados recientemente al portal</h2>
+      <div class="row">
+        @foreach ($discos as $disco)
+          <div class="col-md-3 mb-4">
+            <a href="{{ route('interprete.album.show', [$disco->interprete->slug, $disco->slug]) }}"
+              class="text-decoration-none">
+              <div class="card">
+                <img class="card-img-top w-100 h-auto object-cover" src="{{ asset('storage/albunes/' . $disco->foto) }}"
+                  alt="{{ $disco->album }}">
+                <div class="card-body">
+                  <h5 class="card-title">{{ $disco->album }}</h5>
+                  <p class="card-text text-gray-500 text-sm">{{ $disco->interprete->interprete }}</p>
+                  <p class="card-text">{{ number_format($disco->visitas, 0, '', ',') }} visitas</p>
+                </div>
+              </div>
+            </a>
           </div>
         @endforeach
       </div>
@@ -268,4 +492,5 @@
     </section> --}}
 
   </div>
+
 @endsection
