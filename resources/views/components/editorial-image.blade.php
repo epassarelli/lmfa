e042a660cc4b9def2bd1656dd3618ac67e069652
Eduardo Passarelli <epassarelli@gmail.com>
2026-08-31 13:15:42 -0300

    feat(media): add reusable editorial image component

---
+++ resources/views/components/editorial-image.blade.php
@@ -0,0 +1,37 @@
+@props([
+    'entity',
+    'variant' => 'card',
+    'class' => '',
+    'loading' => 'lazy',
+    'fetchpriority' => 'auto',
+    'sizes' => '100vw',
+    'minimal' => false,
+])
+
+@inject('editorialImageResolver', 'App\\Services\\EditorialImageResolver')
+
+@php
+    $resolvedImage = $editorialImageResolver->resolve($entity);
+@endphp
+
+@if ($resolvedImage->isMedia())
+    <x-optimized-image
+        :image="$resolvedImage->media"
+        :variant="$variant"
+        :class="$class"
+        :alt="$resolvedImage->alt"
+        :loading="$loading"
+        :fetchpriority="$fetchpriority"
+        :sizes="$sizes"
+        :minimal="$minimal"
+    />
+@else
+    <img
+        src="{{ $resolvedImage->url }}"
+        alt="{{ $resolvedImage->alt }}"
+        class="{{ $class }}"
+        loading="{{ $loading }}"
+        fetchpriority="{{ $fetchpriority }}"
+        decoding="async"
+    >
+@endif
