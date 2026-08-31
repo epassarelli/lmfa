946f440488dc282b0685e43c282b20498a2c1902
Eduardo Passarelli <epassarelli@gmail.com>
2026-08-31 20:47:06 -0300

    feat(recipes): expose structured fields and casts

--- app/Models/Comida.php
+++ app/Models/Comida.php
@@ -11,14 +11,35 @@ class Comida extends Model
     use CommonMethodsTrait;
     use HasFactory;
 
+    protected $casts = [
+        'ingredients' => 'array',
+        'instructions' => 'array',
+        'publicar' => 'datetime',
+        'visitas' => 'integer',
+        'estado' => 'integer',
+        'prep_time_minutes' => 'integer',
+        'cook_time_minutes' => 'integer',
+    ];
+
     protected $fillable = [
         'titulo',
-        'receta',
-        'foto',
         'slug',
+        'receta',
+        'excerpt',
+        'ingredients',
+        'instructions',
+        'prep_time_minutes',
+        'cook_time_minutes',
+        'servings',
+        'region',
+        'seo_title',
+        'meta_description',
+        'foto',
+        'image_alt',
         'publicar',
         'visitas',
         'estado',
+        'user_id',
     ];
 
     public function images()
